589182b1e9460f0d54c9a6653587d7fbb6d29940
JoungMinJu <alswn9938@gmail.com>
2021-10-26 00:42:24 +0900

    [21.10.26 정민주] 수정

--- dormline/urls.py
+++ dormline/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name='dormline'
 urlpatterns = [
     path('main/',dorm_main,name="dorm_main"),
-    path('view_random/<str:username>', view_random, name='view_random'),
-    path('follow/<str:username>',follow, name='follow'),
+    path('view_random/<str:id>', view_random, name='view_random'),
+    path('follow/<str:id>',follow, name='follow'),
 ] 
--- dormline/views.py
+++ dormline/views.py
@@ -12,14 +12,14 @@ def dorm_main(request):
     followers=request.user.d_followers
     return render(request, 'dorm_main.html',{'followers':followers})
 
-def view_random(request, username):
-    me=User.objects.get(username=username)
-    me=int(me.dorm_id)
-    floor=me//1000
-    room=me%1000
+def view_random(request, id):
+    me=User.objects.get(id=id)
+    me_id=int(me.dorm_id)
+    floor=me_id//1000
+    room=me_id%1000
     floor=str(floor)
     #내 층이랑 방 호수 받았음.
-    others=User.objects.filter(Q(d_followers__isnull=True)&Q(dorm_id__startswith=floor)&~Q(dorm_id=me)&~Q(dorm_id=0)&~Q(dorm_id='')&~Q(d_followings=me))
+    others=User.objects.filter(Q(d_followers__isnull=True)&Q(dorm_id__startswith=floor)&~Q(dorm_id=me_id)&~Q(dorm_id=0)&~Q(dorm_id='')&~Q(d_followings__in=[request.User]))
     #others는 층수 같고, 내 방은 아닌 사람
     #dorm_id가 0은 아니어야함 공백도 아니어야함
     #팔로워도 없어야한다. 
@@ -35,9 +35,9 @@ def view_random(request, username):
     return render(request, 'dorm_rand.html',{'others':others})
 
  
-def follow(request, username):
-    me=User.objects.get(username=request.user.username)
-    target=User.objects.get(username=username)
+def follow(request, id):
+    me=User.objects.get(id=request.user.id)
+    target=User.objects.get(id=id)
     target.d_followers.add(me)
     return redirect('dormline:dorm_main')
 
