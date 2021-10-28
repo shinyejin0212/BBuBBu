02ae1378de66813a63de89f586e4d3da97323d01
MINJU <alswn9938@gmail.com>
2021-10-29 03:52:31 +0900

    긱라인

--- matching/models.py
+++ matching/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib import auth
+
+# class Match(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     followings = models.ManyToManyField("self", related_name="school_followers", symmetrical=False)
+
+# @receiver(post_save, sender=User)
+# def create_user_match(sender, instance, created, **kwargs):
+#     if created:
+#         Match.objects.create(user=instance)
+
+
+# @receiver(post_save, sender=User)
+# def save_user_match(sender, instance, **kwargs):
+#     instance.match.save()
